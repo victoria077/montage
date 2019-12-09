0637928f5ebcc61e46ee8d8ffec4a854c1a42541
viktoria <vik.slepchenko@gmail.com>
2019-12-09 22:11:24 +0100

    fixed script for buttons in items section

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-var btn = document.createElement("BUTTON");
+let btn = document.createElement("BUTTON");
 btn.innerHTML = "Заказать монтаж";
 btn.classList.add("_customer-order");
 
 var items = document.getElementsByClassName("_customer-item");
-
+var model = document.getElementsByClassName("_customer-model");
+var forms = document.getElementsByClassName("item-wrap");
+var button =  document.getElementsByClassName("_customer-order");
 var onHover = function(event) {
   for (var i = 0; i < items.length; i++) {
     event.currentTarget.style.boxShadow =
@@ -11,8 +13,13 @@ var onHover = function(event) {
     event.currentTarget.style.padding = "20px";
     event.currentTarget.style.borderRadius = "10px";
     event.currentTarget.style.transition = "ease 0.3s";
-    event.currentTarget.append(btn);
+    if(event.currentTarget.className == "_customer-item")
+    {
+    event.currentTarget.insertBefore(btn, items.lastElementChild);
   }
+
+  }
+
 };
 
 var onHoverOut = function(event) {
@@ -20,14 +27,18 @@ var onHoverOut = function(event) {
     event.currentTarget.style.boxShadow = "none";
     event.currentTarget.style.padding = "0px";
     event.currentTarget.style.borderRadius = "0px";
-    event.currentTarget.removeChild(btn)
-  }
+    }
+    if(event.relatedTarget.className == '_customer-items' || event.relatedTarget.className == '_customer-container'){
+      btn.remove();
+    }
+
 };
 
+
 for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener("mouseover", onHover, false);
+  items[i].addEventListener("mouseover", onHover, true);
 }
 
 for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener("mouseout", onHoverOut, false);
+  items[i].addEventListener("mouseout", onHoverOut, true);
 }
